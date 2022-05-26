c48df04220b2bb3373e868e76f28dba930f76b04
ikhjhj2k3 <106287481+ikhjhj2k3@users.noreply.github.com>
2022-05-26 15:23:48 +0900

    Update SpringmvcApplication.java

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringmvcApplication {
-
+//SpringBootApplication_test1
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
 	}
