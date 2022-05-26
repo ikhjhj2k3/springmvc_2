a86fe2b66a438fab2cb285d5bf09efa1f8bd5ce8
jungsw@asianaidt.com <gkswls3209#>
2022-05-26 15:41:57 +0900

    test2

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringmvcApplication {
 	
-//SpringBootApplication_test11
+//SpringBootApplication_test12
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
