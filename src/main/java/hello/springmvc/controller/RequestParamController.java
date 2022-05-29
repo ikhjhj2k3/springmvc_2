77c9d6e613d557070750bce341f8783347f42f40
jungsw@asianaidt.com <gkswls3209#>
2022-05-30 08:57:33 +0900

    JSON 반영

--- src/main/java/hello/springmvc/controller/RequestParamController.java
+++ src/main/java/hello/springmvc/controller/RequestParamController.java
@@ -1,5 +1,6 @@
 package hello.springmvc.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.springmvc.basic.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,56 @@ import java.nio.charset.StandardCharsets;
 @Controller
 
 public class RequestParamController {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/json-v1")
+    public void  requestBodyJsonV1(HttpServletRequest request,
+                                   HttpServletResponse response) throws IOException {
+        ServletInputStream servletInputStream = request.getInputStream();
+        String body = StreamUtils.copyToString(servletInputStream, StandardCharsets.UTF_8);
+
+        UserInfo userInfo = objectMapper.readValue(body, UserInfo.class);
+
+
+        log.error("body = {}", body);
+        log.error("name = {}, age={}", userInfo.getName(), userInfo.getAge());
+
+        response.getWriter().write("called");
+    }
+
+    @PostMapping("/json-v2")
+    @ResponseBody
+    public String  requestBodyJsonV2(
+                                   @RequestBody String body
+                                   ) throws IOException {
+
+
+        UserInfo userInfo = objectMapper.readValue(body, UserInfo.class);
+
+
+        log.error("body = {}", body);
+        log.error("name = {}, age={}", userInfo.getName(), userInfo.getAge());
+        return "requestBodyJsonV2";
+
+    }
+
+    @PostMapping("/json-v3")
+    @ResponseBody
+    public String  requestBodyJsonV3(
+            @RequestBody UserInfo userInfo
+    ) throws IOException {
+
+
+        //UserInfo userInfo = objectMapper.readValue(body, UserInfo.class);
+
+
+        log.error("body = {}", userInfo.toString());
+        log.error("name = {}, age={}", userInfo.getName(), userInfo.getAge());
+        return "requestBodyJsonV2";
+
+    }
+
 
     @RequestMapping("")
     public void requestParamV1(HttpServletRequest request,
